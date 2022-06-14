2a29279431e9bb6b1bb5d4bbc44349b0d5e2638f
Martijn Bentum <bentummartijn@gmail.com>
2022-06-14 17:03:46 +0200

    typo fix

--- istanbul/settings.py
+++ istanbul/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'installations.apps.InstallationConfig',
+	'installations.apps.InstallationsConfig',
 	'django_select2',
 ]
 
