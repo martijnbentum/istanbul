97ad0509d45cdda7e3f01f54be73ecb80b7cf9f1
Martijn Bentum <bentummartijn@gmail.com>
2022-07-25 15:07:06 +0200

    layout change

--- utilities/urls.py
+++ utilities/urls.py
@@ -5,9 +5,11 @@ from . import views
 
 app_name = 'utilities'
 urlpatterns = [
-	path('overview/',views.overview,name='overview'),
+	# path('overview/',views.overview,name='overview'),
 	path('close/',views.close,name='close'),
-	path('list_view/<str:model_name>/<str:app_name>/',views.list_view,name='list_view'),
-	path('list_view/<str:model_name>/<str:app_name>/<int:max_entries>',views.list_view,
+	path('list_view/<str:model_name>/<str:app_name>/',views.list_view,
 		name='list_view'),
+	path('list_view/<str:model_name>/<str:app_name>/<int:max_entries>',
+		views.list_view, name='list_view'),
+		
 ]
