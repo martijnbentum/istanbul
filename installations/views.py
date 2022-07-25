02a16c42829aa10eda4083edf1d333fd5fcade9c
Martijn Bentum <bentummartijn@gmail.com>
2022-07-25 16:59:00 +0200

    added systeminstallation_formset

--- installations/views.py
+++ installations/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import System
-from .forms import SystemForm
+from .forms import SystemForm 
+from .forms import systeminstallation_formset, installationsystem_formset
 from utilities.views import edit_model
 
 def hello_world(request):
@@ -8,7 +9,7 @@ def hello_world(request):
 
 # Create your views here.
 def edit_system(request, pk = None, focus = '', view = 'complete'):
-	names = ''
+	names = 'systeminstallation_formset'
 	return edit_model(request, __name__, 'System','installations',pk,
 		formset_names = names, focus = focus, view = view)
 
