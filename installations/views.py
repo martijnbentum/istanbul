b0c3d26758876313f6c97232bb8247e222ab881b
Martijn Bentum <bentummartijn@gmail.com>
2022-07-25 12:09:29 +0200

    added first views

--- installations/views.py
+++ installations/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import System
+from .forms import SystemForm
+from utilities.views import edit_model
+
+def hello_world(request):
+	return render(request,'installations/hello_world.html')
 
 # Create your views here.
+def edit_system(request, pk = None, focus = '', view = 'complete'):
+	names = ''
+	return edit_model(request, __name__, 'System','installations',pk,
+		formset_names = names, focus = focus, view = view)
