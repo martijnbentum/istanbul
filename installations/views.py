7b19403eb6a193dd7aa077ccba1faee6e51d100e
Martijn Bentum <bentummartijn@gmail.com>
2022-07-25 15:07:25 +0200

    added edit views

--- installations/views.py
+++ installations/views.py
@@ -11,3 +11,48 @@ def edit_system(request, pk = None, focus = '', view = 'complete'):
 	names = ''
 	return edit_model(request, __name__, 'System','installations',pk,
 		formset_names = names, focus = focus, view = view)
+
+def edit_installation(request, pk = None, focus = '', view = 'complete'):
+	names = ''
+	return edit_model(request, __name__, 'Installation','installations',pk,
+		formset_names = names, focus = focus, view = view)
+
+def edit_person(request, pk = None, focus = '', view = 'complete'):
+	names = ''
+	return edit_model(request, __name__, 'Person','installations',pk,
+		formset_names = names, focus = focus, view = view)
+
+def edit_institution(request, pk = None, focus = '', view = 'complete'):
+	names = ''
+	return edit_model(request, __name__, 'Institution','installations',pk,
+		formset_names = names, focus = focus, view = view)
+
+def edit_religion(request, pk = None, focus = '', view = 'complete'):
+	names = ''
+	return edit_model(request, __name__, 'Religion','installations',pk,
+		formset_names = names, focus = focus, view = view)
+
+def edit_event(request, pk = None, focus = '', view = 'complete'):
+	names = ''
+	return edit_model(request, __name__, 'Event','installations',pk,
+		formset_names = names, focus = focus, view = view)
+
+def edit_literature(request, pk = None, focus = '', view = 'complete'):
+	names = ''
+	return edit_model(request, __name__, 'Literature','installations',pk,
+		formset_names = names, focus = focus, view = view)
+
+def edit_image(request, pk = None, focus = '', view = 'complete'):
+	names = ''
+	return edit_model(request, __name__, 'Image','installations',pk,
+		formset_names = names, focus = focus, view = view)
+
+def edit_figure(request, pk = None, focus = '', view = 'complete'):
+	names = ''
+	return edit_model(request, __name__, 'Figure','installations',pk,
+		formset_names = names, focus = focus, view = view)
+
+def edit_style(request, pk = None, focus = '', view = 'complete'):
+	names = ''
+	return edit_model(request, __name__, 'Style','installations',pk,
+		formset_names = names, focus = focus, view = view)
