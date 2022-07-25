aac1b679ef8dfe4ab4b36d663dadc1325bb7fbeb
Martijn Bentum <bentummartijn@gmail.com>
2022-07-25 15:05:48 +0200

    added account utilities and installations

--- istanbul/urls.py
+++ istanbul/urls.py
@@ -18,5 +18,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+	path('accounts/',include('accounts.urls')),
+	path('accounts/',include('django.contrib.auth.urls')),
+	path('utilities/',include('utilities.urls')),
+	path ('installations/',include('installations.urls')),
 	path ('',include('installations.urls')),
 ]
